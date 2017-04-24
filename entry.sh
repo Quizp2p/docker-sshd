4e30845a013dc735cfffe528b2030f72623fa5d1
Quiz42 <quizzhu@gmail.com>
2017-04-24 10:20:41 +0800

    Fix again

--- entry.sh
+++ entry.sh
@@ -10,6 +10,6 @@ echo -e 'y/n'|ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa -q
 sshpass -f /root/.ssh/password.txt ssh-copy-id -o StrictHostKeyChecking=no $REMOTE_SRV
 
 # Connect to remote server
-ssh -N -R $REMOTE_PORT:127.0.0.1:LOCAL_PORT $REMOTE_SRV
+ssh -N -R $REMOTE_PORT:127.0.0.1:$LOCAL_PORT $REMOTE_SRV
 
 
